3087fca87a1554cc0a95907dff8e3217a6d5596d
Yun Chenxi <cleoxiii@163.com>
2020-08-06 01:18:15 +0800

    [yunchenxi] feat: should get user list, and only add new rs event when user alreay exists.

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.domain.RsEvent;
 import com.thoughtworks.rslist.domain.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,6 @@ public class UserController {
         userList.add(user);
     }
 
-
+    @GetMapping("/user/list")
+    public List<User> getUserList() { return userList; }
 }
--- src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/UserControllerTest.java
@@ -11,7 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -64,5 +68,33 @@ public class UserControllerTest {
 
         mockMvc.perform(post("/user").content(jsonString).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void should_add_rs_event_when_user_already_exist() throws Exception {
+        String jsonString = "{\"eventName\":\"猪肉涨价啦\",\"keyWord\":\"经济\"}," +
+                "\"user\":{\"userName\":\"admin\",\"gender\":\"female\",\"age\":22,\"email\":\"a@b.com\",\"phone\":\"12345678900\"}";
+
+        mockMvc.perform(post("/rs/event").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[3].eventName", is("猪肉涨价啦")))
+                .andExpect(jsonPath("$[3].keyWord", is("经济")))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/user/list"))
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].userName", is("admin")))
+                .andExpect(jsonPath("$[0].gender", is("female")))
+                .andExpect(jsonPath("$[0].age", is(22)))
+                .andExpect(jsonPath("$[0].email", is("a@b.com")))
+                .andExpect(jsonPath("$[0].phone", is("12345678900")))
+                .andExpect(status().isOk());
     }
 }

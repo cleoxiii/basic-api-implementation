f3560004691a3c5f3226bf7828f14e22548438be
Yun Chenxi <cleoxiii@163.com>
2020-08-06 19:38:32 +0800

    [yunchenxi] test: finish demo, test return value of rs/list does not contain user.

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -36,10 +36,13 @@ class RsControllerTest {
                 .andExpect(jsonPath("$", hasSize(3)))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
                 .andExpect(jsonPath("$[0].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[0].user", not(hasKey("admin"))))
                 .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
                 .andExpect(jsonPath("$[1].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[1].user", not(hasKey("admin"))))
                 .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
                 .andExpect(jsonPath("$[2].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[2].user", not(hasKey("admin"))))
                 .andExpect(status().isOk());
     }
 
@@ -48,6 +51,7 @@ class RsControllerTest {
         mockMvc.perform(get("/rs/1"))
                 .andExpect(jsonPath("$.eventName", is("第一条事件")))
                 .andExpect(jsonPath("$.keyWord", is("无标签")))
+                .andExpect(jsonPath("$.user", not(hasKey("admin"))))
                 .andExpect(status().isOk());
     }
 
